f27a571624747ed77bff3dad8375aae92ec8c273
Toavina <thleong3@gmail.com>
2025-05-19 19:30:49 +0300

    Chat

---
+++ main.py
@@ -0,0 +1,81 @@
+import os
+import getpass
+
+from dotenv import load_dotenv
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_unstructured import UnstructuredLoader
+from langchain_community.vectorstores import Chroma
+from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline, HuggingFaceEmbeddings
+from transformers import pipeline
+
+import gradio as gr
+
+
+load_dotenv()
+
+if not os.getenv("HUGGINGFACEHUB_API_TOKEN"):
+    os.environ["HUGGINGFACEHUB_API_TOKEN"] = getpass.getpass("Enter your token: ")
+
+EMBEDDING_MODEL = "sentence-transformers/all-mpnet-base-v2"
+CHAT_MODEL = "Qwen/Qwen3-4B"
+
+PROMPT_TEMPLATE="""\
+    ### Instruction
+    Answer the question by leveraging the information in the following context
+
+    ### Context
+    %s
+
+    ### Question
+    %s
+
+    ### Output
+"""
+
+huggingface_embedding = HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL)
+llm_model = ChatHuggingFace(
+    llm=HuggingFacePipeline(
+        pipeline=pipeline("text-generation", model=CHAT_MODEL)
+    )
+)
+
+def inference(prompt, files):
+
+    chunks = UnstructuredLoader(files).load_and_split(
+        RecursiveCharacterTextSplitter(
+            chunk_size=100,
+            chunk_overlap=20,
+            length_function=len,
+        )
+    )
+    
+
+    vectordb  = Chroma.from_documents(chunks, huggingface_embedding)
+    retriever = vectordb.as_retriever(search_type='mmr', search_kwargs={"k": 3})
+
+    docs = retriever.invoke(prompt)
+
+    context = "\n".join(docs)
+
+    return llm_model.invoke(PROMPT_TEMPLATE % (context, prompt))
+
+
+interface = gr.Interface(
+    title="QA Bot",
+    description="Ask questions based on your uploaded documents.",
+    fn=inference,
+    inputs=[
+        gr.Textbox(lines=2, label="Question"),
+        gr.File(file_count="multiple", label="Upload Documents")
+    ],
+    outputs=gr.Textbox(label="Answer"),
+)
+
+
+def main():
+    interface.launch(server_name="127.0.0.1", server_port= 7860)
+
+
+if __name__ == "__main__":
+    main()
